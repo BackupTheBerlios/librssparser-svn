7c429ad7b0aca8946fae14b2cc0729bc23daed67
dienet <dienet@bced26ef-f862-0410-b84b-f60364e793c8>
2009-02-16 18:27:52 +0000

    vim

---
+++ trunk/src/rss_options.h
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ * 
+ *       Filename:  rss_options.h
+ * 
+ *    Description:  Simple rss parser upon expat
+ * 
+ *        Version:  1.0
+ *        Created:  16.02.2009 19:18:40 CET
+ *       Revision:  none
+ *       Compiler:  gcc
+ * 
+ *         Author:  Slawomir Stepien (dienet@poczta.fm), 
+ *        Company:  
+ * 
+ * =====================================================================================
+ */
+
+#ifndef  RSS_OPTIONS_INC
+#define  RSS_OPTIONS_INC
+
+struct opt
+{
+	int linked_list; 
+};
+
+#endif   /* ----- #ifndef RSS_OPTIONS_INC  ----- */
+
